872fb4da672eab3f45e348d8146591e505c7f108
HuiLanAi <310-we-aaa-1@163.com>
2017-11-07 16:38:10 +0800

    io

---
+++ Defination.h
@@ -0,0 +1,47 @@
+﻿#pragma once
+
+#define INT 1
+#define LONG_INT 2
+#define FLOAT 3
+#define DOUBLE 4
+#define VOID 5
+/* 类型 */
+
+#define ADD 6
+#define MINUS 7
+#define MULT 8
+#define DIV 9
+/* 算术运算符 */
+
+#define AND 10
+#define OR 11
+#define NOT 12
+#define LESS 13
+#define MORE 14
+#define LESS_EQU 15
+#define MORE_EQU 16
+#define DOUBLE_EQU 17
+/* 逻辑运算符 */
+
+#define SYNX 18
+#define NUMBER 19
+#define USE_FUNCTION 20
+/* 变量名 常数 函数调用 */
+
+#define EQUAL 21
+#define LINGER 22
+#define LEFT 23
+#define RIGHT 24
+#define LEFT_BRACE 25
+#define RIGHT_BRACE 26
+/* 其他符号 */
+
+#define IF 27
+#define ELSE 28
+#define WHILE 29
+#define RETURN 30
+/* 其他关键字 */
+
+
+
+
---
+++ Sentence.h
@@ -0,0 +1,45 @@
+﻿#pragma once
+
+#include "iostream"
+#include "vector"
+#include "string"
+using namespace std;
+
+#include "Defination.h"
+
+class Sentence
+{
+    string fileName;//所在文件
+    int line;//所在行数
+    string thisLine;
+    vector<string> sentencePart;
+    vector<int> partType;
+
+    Sentence()
+    /* 默认构造函数 */
+    {
+        ;
+    }
+
+    Sentence(int l, string f, string s)
+    /* 初始化行数和内容 */
+    {
+        line = l;
+        thisLine = s;
+        fileName = f;
+    }
+
+    void partition()
+    {
+        ;
+        /* 填入朱强所写的代码 */
+    }
+
+    int anaPart()
+    {
+        /* 如果产生词法错误则在此函数中报错 */
+        /* 东哥写的 */
+    }
+
+
+};
