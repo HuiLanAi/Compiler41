715c4f916766b4d8f33d1fef6a724ea843c1f98f
HuiLanAi <310-we-aaa-1@163.com>
2017-11-07 23:26:04 +0800

    添加了文件读取，在LoadFile.h中

---
+++ LoadFile.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include "fstream"
+#include "string"
+#include "iostream"
+using namespace std;
+
+void readFile(string *filename, string* fileContent)
+{
+    cout << "请输入文件名：" << endl;
+    cin >> *filename;
+    
+    ifstream in;
+    in.open(*filename);
+	char temp[524288] = { 0 };
+    in.getline(temp, 524288, 0);
+	(*fileContent).append(temp);
+}
--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 ﻿#include "iostream"
+#include "string"
 using namespace std;
 
 #include "Sentence.h"
 #include "Defination.h"
+#include "LoadFile.h"
 
 int main()
 {
-
+    string filename, fileContent;
+	readFile(&filename, &fileContent);
+    cout << fileContent << endl;
+	
+	system("pause");
 }
