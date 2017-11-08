ebfca2207b786f4d06825618a319b483c93b48a5
Alex-No-Bug <3068779985@qq.com>
2017-11-08 15:42:39 +0800

    加入了partition

--- Sentence.h
+++ Sentence.h
@@ -9,7 +9,18 @@
 using namespace std;
 
 #include "Defination.h"
-
+//partition的所用变量
+    //判断是否是括号
+    bool isBracket(char a);
+    int  isLogic(char a);//   0是（）{}   1是-  2是算符  3不是算符
+    int isNumber(char a);
+    //判断字母  下划线
+    bool isLetter(char a);
+    
+     //前缀符
+    string prefix[] = { "-",  "+","*","/","=",">","<","&","|", "," ,"!" ,   "(" ,")" ,"{","}" ,"'","#","@","$","%","\\","?",":","_" };//11后是括号
+    //数字符
+    string numberPrefix[] = { "-","0","1","2","3","4","5","6","7","8","9" };
 class Sentence
 {
 public:
@@ -18,6 +29,7 @@ public:
     string thisLine;
     vector<string> sentencePart;
     vector<int> partType;
+
 
     Sentence()
     /* 默认构造函数 */
@@ -33,11 +45,7 @@ public:
         fileName = f;
     }
 
-    void partition()
-    {
-        ;
-        /* 填入朱强所写的代码 */
-    }
+    void partition();
 
     int anaPart()
     {
@@ -47,6 +55,78 @@ public:
 
 
 };
+
+void Sentence:: partition()
+{
+
+        int i = 0, j = 0;//2个指针
+        thisLine += '\0';
+        while (thisLine[i] != '\n')
+        {
+    
+            string s = "";
+            //处理空格
+            while (thisLine[i] == ' ')
+            {
+                i++;
+                j++;
+            }
+            //数字类型  开头就是数字
+            if (isNumber(thisLine[j]) == 2)
+            {
+                if ((isNumber(thisLine[j]) == 1 && isLogic(thisLine[j - 1]) == 2))
+                {
+                    s += thisLine[j++];//如果是负数  就先将-号压入
+                }
+                while (thisLine[j] != '\n'&& thisLine[j] != ' ' && ((isNumber(thisLine[j]) == 2) || thisLine[j] == '.'))//之后是数字  或者小数点
+                {
+                    s += thisLine[j++];
+                }
+                sentencePart.push_back(s);
+                i = j;
+                s = "";
+            }
+    
+            //变量  关键字
+            else if (isLetter(thisLine[j]))
+            {
+                while (thisLine[j] != '\n'&&thisLine[j] != ' ' && isLetter(thisLine[j]))
+                {
+                    s += thisLine[j++];
+                }
+                sentencePart.push_back(s);
+                i = j;
+                s = "";
+            }
+    
+            //算符                                                    减号                                 括号
+            else if (isLogic(thisLine[j]) == 2 || isLogic(thisLine[j]) == 1 || isLogic(thisLine[j]) == 0)
+            {
+                //减号
+                if (isLogic(thisLine[j] == 1))
+                    s = +thisLine[j++];
+    
+                //是括号
+                else	if (isLogic(thisLine[j]) == 0)
+                {
+                    s = +thisLine[j++];
+                    
+                }
+    
+                //普通算符
+                else {
+                    while (thisLine[j] != '\n' && thisLine[j] != ' ' && (isLogic(thisLine[j]) == 2))
+                    {
+                        s += thisLine[j++];
+                    }
+                }
+                sentencePart.push_back(s);//核心
+                i = j;
+                s = "";
+            }
+        }
+}
+
 
 void mergeString(vector<string>* str1, vector<int>* type, vector<Sentence> array)
 /*  将文本中所有行的词法单元和种类都合并到一起 */
@@ -61,4 +141,72 @@ void mergeString(vector<string>* str1, vector<int>* type, vector<Sentence> array
             (*type).push_back(array.at(i).partType.at(j));
         }
     }
+}
+
+
+//判断是否是括号
+bool isBracket(char a)
+{
+string s = "";
+s = +a;
+for (int i = 11; i<15; i++)
+{
+if (s.compare(prefix[i]))
+return(true);
+}
+return(false);
+}
+
+int  isLogic(char a)//   0是（）{}   1是-  2是算符  3不是算符
+{
+//属于标识符和逗号是false
+string s = "";
+s = +a;
+if (s.compare(prefix[0]) == 0)  return(1);//负号
+for (int i = 11; i<19; i++)
+{
+if (s.compare(prefix[i]))
+{
+return(2);
+}
+}
+for (int i = 1; i<11; i++)//正常算符
+{
+if (s.compare(prefix[i]) == 0)
+{
+return(2);
+}
+}
+
+for (int i = 11; i < 15; i++)//判断括号
+{
+if (s.compare(prefix[i]) == 0)
+return(0);
+}
+return(3);//不是算符
+}
+
+//判断开头是不是数字 -号返回1  其他返回2 不是返回三
+int isNumber(char a)
+{
+string s = "";
+s = +a;
+if (s.compare(numberPrefix[0]) == 0)return(1);
+for (int i = 1; i<11; i++)
+{
+if (s.compare(numberPrefix[i]) == 0)
+{
+return(2);
+}
+}
+return(3);
+}
+
+//判断字母  下划线
+bool isLetter(char a)
+{
+if ((a >= 'a'&&a <= 'z') || (a >= 'A'&&a <= 'Z') || a == '_')
+return(true);
+else
+return(false);
 }
