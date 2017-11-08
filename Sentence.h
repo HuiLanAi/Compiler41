a2907ca317c19a0b379e58dd2d163f73da89b902
HuiLanAi <310-we-aaa-1@163.com>
2017-11-08 19:55:52 +0800

    test.cpp是用来测试的

--- Sentence.h
+++ Sentence.h
@@ -10,17 +10,15 @@ using namespace std;
 
 #include "Defination.h"
 //partition的所用变量
-    //判断是否是括号
-    bool isBracket(char a);
+    bool isBracket(char a);//判断是否是括号
     int  isLogic(char a);//   0是（）{}   1是-  2是算符  3不是算符
     int isNumber(char a);
     //判断字母  下划线
-    bool isLetter(char a);
-    
-     //前缀符
+    bool isLetter(char a); //前缀符
     string prefix[] = { "-",  "+","*","/","=",">","<","&","|", "," ,"!" ,   "(" ,")" ,"{","}" ,"'","#","@","$","%","\\","?",":","_" };//11后是括号
     //数字符
     string numberPrefix[] = { "-","0","1","2","3","4","5","6","7","8","9" };
+
 class Sentence
 {
 public:
@@ -45,78 +43,10 @@ public:
         fileName = f;
     }
 
-    void partition();
-    void showThisLine();
-
-    int anaPart()
-    {
-        int i;
-        for(i=0;i<sentencePart.size();i++)
-        {
-    
-        //判断关键字,运算符
-            if(sentencePart[i]=="int")
-                 sentencePart.push_back(INT);
-            else if(sentencePart[i]=="long")
-                 sentencePart.push_back(LONG_INT);
-            else if(sentencePart[i]=="float")
-                 sentencePart.push_back(FLOAT);
-            else if(sentencePart[i]=="double")
-                 sentencePart.push_back(DOUBLE);
-            else if(sentencePart[i]=="void")
-                 sentencePart.push_back(VOID);
-            else if(sentencePart[i]=="+")
-                 sentencePart.push_back(ADD);
-            else if(sentencePart[i]=="-")
-                 sentencePart.push_back(MINUS);
-            else if(sentencePart[i]=="*")
-                 sentencePart.push_back(MULT);
-            else if(sentencePart[i]=="/")
-                sentencePart.push_back(DIV);
-            else if(sentencePart[i]=="&")
-                 sentencePart.push_back(ADD);
-            else if(sentencePart[i]=="|")
-                 sentencePart.push_back(OR);
-            else if(sentencePart[i]=="!")
-                 sentencePart.push_back(NOT);
-            else if(sentencePart[i]=="<")
-                 sentencePart.push_back(LESS);
-            else if(sentencePart[i]==">")
-                 sentencePart.push_back(MORE);
-            else if(sentencePart[i]=="<=")
-                 sentencePart.push_back(LESS_EQU);
-            else if(sentencePart[i]==">=")
-                 sentencePart.push_back(MORE_EQU);
-            else if(sentencePart[i]=="==")
-                 sentencePart.push_back(DOUBLE_EQU);
-            else if(sentencePart[i]=="=")
-                 sentencePart.push_back(EQUAL);
-            else if(sentencePart[i]==",")
-                 sentencePart.push_back(LINGER);
-            else if(sentencePart[i]=="(")
-                 sentencePart.push_back(LEFT);
-            else if(sentencePart[i]==")")
-                 sentencePart.push_back(RIGHT);
-            else if(sentencePart[i]=="{")
-                 sentencePart.push_back(LEFT_BRACE);
-            else if(sentencePart[i]=="}")
-                 sentencePart.push_back(RIGHT_BRACE);
-            else if(sentencePart[i]=="if")
-                 sentencePart.push_back(IF);
-            else if(sentencePart[i]=="else")
-                 sentencePart.push_back(ELSE);
-            else if(sentencePart[i]=="while")
-                 sentencePart.push_back(WHILE);
-            else if(sentencePart[i]=="return")
-                 sentencePart.push_back(RETURN);
-            else if(sentencePart[i]=="'")
-                 sentencePart.push_back(SINGLE_QOUTE);
-            else if(sentencePart[i]=="\"")
-                 sentencePart.push_back(DOUBLE_QOUTE);
-        /* 如果产生词法错误则在此函数中报错 */
-        /* 东哥写的 */
-    }
-
+    void partition();//分割句子里的各个成分并填入sentencePart
+    void showThisLine();//输出thisLine
+    int anaPart();//词法分析主函数
+    void showSentencePart();//输出sentencePart
 
 };
 
@@ -124,10 +54,9 @@ void Sentence:: partition()
 {
 
         int i = 0, j = 0;//2个指针
-        thisLine += '\0';
-        while (thisLine[i] != '\n')
+        //thisLine += '\0';
+        while (i < thisLine.size())
         {
-    
             string s = "";
             //处理空格
             while (thisLine[i] == ' ')
@@ -196,6 +125,14 @@ void Sentence:: showThisLine()
     cout << thisLine << endl;
 }
 
+void Sentence:: showSentencePart()
+{
+    for(int i = 0; i < sentencePart.size(); i++)
+    {
+        cout << sentencePart.at(i) << endl;
+    }
+}
+
 
 void mergeString(vector<string>* str1, vector<int>* type, vector<Sentence> array)
 /*  将文本中所有行的词法单元和种类都合并到一起 */
@@ -212,70 +149,140 @@ void mergeString(vector<string>* str1, vector<int>* type, vector<Sentence> array
     }
 }
 
+// int Sentence::anaPart()
+// {
+//     int i;
+//     for(i=0;i<sentencePart.size();i++)
+//     {
+
+//     //判断关键字,运算符
+//         if(sentencePart[i]=="int")
+//              sentencePart.push_back(INT);
+//         else if(sentencePart[i]=="long")
+//              sentencePart.push_back(LONG_INT);
+//         else if(sentencePart[i]=="float")
+//              sentencePart.push_back(FLOAT);
+//         else if(sentencePart[i]=="double")
+//              sentencePart.push_back(DOUBLE);
+//         else if(sentencePart[i]=="void")
+//              sentencePart.push_back(VOID);
+//         else if(sentencePart[i]=="+")
+//              sentencePart.push_back(ADD);
+//         else if(sentencePart[i]=="-")
+//              sentencePart.push_back(MINUS);
+//         else if(sentencePart[i]=="*")
+//              sentencePart.push_back(MULT);
+//         else if(sentencePart[i]=="/")
+//             sentencePart.push_back(DIV);
+//         else if(sentencePart[i]=="&")
+//              sentencePart.push_back(ADD);
+//         else if(sentencePart[i]=="|")
+//              sentencePart.push_back(OR);
+//         else if(sentencePart[i]=="!")
+//              sentencePart.push_back(NOT);
+//         else if(sentencePart[i]=="<")
+//              sentencePart.push_back(LESS);
+//         else if(sentencePart[i]==">")
+//              sentencePart.push_back(MORE);
+//         else if(sentencePart[i]=="<=")
+//              sentencePart.push_back(LESS_EQU);
+//         else if(sentencePart[i]==">=")
+//              sentencePart.push_back(MORE_EQU);
+//         else if(sentencePart[i]=="==")
+//              sentencePart.push_back(DOUBLE_EQU);
+//         else if(sentencePart[i]=="=")
+//              sentencePart.push_back(EQUAL);
+//         else if(sentencePart[i]==",")
+//              sentencePart.push_back(LINGER);
+//         else if(sentencePart[i]=="(")
+//              sentencePart.push_back(LEFT);
+//         else if(sentencePart[i]==")")
+//              sentencePart.push_back(RIGHT);
+//         else if(sentencePart[i]=="{")
+//              sentencePart.push_back(LEFT_BRACE);
+//         else if(sentencePart[i]=="}")
+//              sentencePart.push_back(RIGHT_BRACE);
+//         else if(sentencePart[i]=="if")
+//              sentencePart.push_back(IF);
+//         else if(sentencePart[i]=="else")
+//              sentencePart.push_back(ELSE);
+//         else if(sentencePart[i]=="while")
+//              sentencePart.push_back(WHILE);
+//         else if(sentencePart[i]=="return")
+//              sentencePart.push_back(RETURN);
+//         else if(sentencePart[i]=="'")
+//              sentencePart.push_back(SINGLE_QOUTE);
+//         else if(sentencePart[i]=="\"")
+//              sentencePart.push_back(DOUBLE_QOUTE);
+//     /* 如果产生词法错误则在此函数中报错 */
+//     /* 东哥写的 */
+// }
 
 //判断是否是括号
 bool isBracket(char a)
 {
-string s = "";
-s = +a;
-for (int i = 11; i<15; i++)
-{
-if (s.compare(prefix[i]))
-return(true);
-}
-return(false);
-}
-
-int  isLogic(char a)//   0是（）{}   1是-  2是算符  3不是算符
-{
-//属于标识符和逗号是false
-string s = "";
-s = +a;
-if (s.compare(prefix[0]) == 0)  return(1);//负号
-for (int i = 11; i<19; i++)
-{
-if (s.compare(prefix[i]))
-{
-return(2);
-}
-}
-for (int i = 1; i<11; i++)//正常算符
-{
-if (s.compare(prefix[i]) == 0)
-{
-return(2);
-}
+    string s = "";
+    s = +a;
+    for (int i = 11; i < 15; i++)
+    {
+        if (s.compare(prefix[i]))
+            return (true);
+    }
+    return (false);
 }
 
-for (int i = 11; i < 15; i++)//判断括号
+int isLogic(char a) //   0是（）{}   1是-  2是算符  3不是算符
 {
-if (s.compare(prefix[i]) == 0)
-return(0);
-}
-return(3);//不是算符
+    //属于标识符和逗号是false
+    string s = "";
+    s = +a;
+    if (s.compare(prefix[0]) == 0)
+        return (1); //负号
+    for (int i = 11; i < 19; i++)
+    {
+        if (s.compare(prefix[i]))
+        {
+            return (2);
+        }
+    }
+    for (int i = 1; i < 11; i++) //正常算符
+    {
+        if (s.compare(prefix[i]) == 0)
+        {
+            return (2);
+        }
+    }
+
+    for (int i = 11; i < 15; i++) //判断括号
+    {
+        if (s.compare(prefix[i]) == 0)
+            return (0);
+    }
+    return (3); //不是算符
 }
 
 //判断开头是不是数字 -号返回1  其他返回2 不是返回三
 int isNumber(char a)
 {
-string s = "";
-s = +a;
-if (s.compare(numberPrefix[0]) == 0)return(1);
-for (int i = 1; i<11; i++)
-{
-if (s.compare(numberPrefix[i]) == 0)
-{
-return(2);
-}
-}
-return(3);
+    string s = "";
+    s = +a;
+    if (s.compare(numberPrefix[0]) == 0)
+        return (1);
+    for (int i = 1; i < 11; i++)
+    {
+        if (s.compare(numberPrefix[i]) == 0)
+        {
+            return (2);
+        }
+    }
+    return (3);
 }
 
 //判断字母  下划线
 bool isLetter(char a)
 {
-if ((a >= 'a'&&a <= 'z') || (a >= 'A'&&a <= 'Z') || a == '_')
-return(true);
-else
-return(false);
+    if ((a >= 'a' && a <= 'z') || (a >= 'A' && a <= 'Z') || a == '_')
+        return (true);
+    else
+        return (false);
 }
--- main.cpp
+++ main.cpp
@@ -9,9 +9,18 @@ using namespace std;
 int main()
 {
     string filename, fileContent;
-    readFile(&filename, &fileContent);
+    readFile(&filename, &fileContent);//读取文件
     vector<Sentence> originFile;//主Sentence数组 
-    partLine(&originFile, filename, fileContent);
+    partLine(&originFile, filename, fileContent);//文本分行写入Sentence
+    for(int i = 0; i < originFile.size(); i++)
+    {
+        originFile.at(i).partition();
+    }//完成分词
+
+    for(int i = 0; i < originFile.size(); i++)
+    {
+        originFile.at(i).showSentencePart();
+    }
 
     system("pause");
     return 0;
